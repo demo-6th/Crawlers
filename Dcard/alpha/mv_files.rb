47dff614d8766b5ab403ce90863cdaa35f802a50
dante <dantecherng@hotmail.com.tw>
2020-12-16 00:19:02 +0800

    clean file

--- Dcard/alpha/mv_files.rb
+++
@@ -1,9 +0,0 @@
-
-def mv_files(f)
-  Dir.mkdir("#{f}")
-  FileUtils.mv('post_id.csv', "#{f}/")
-  FileUtils.mv('post_content.csv', "#{f}/")
-  FileUtils.mv('post_comment_new.csv', "#{f}/")
-  end
-
-
